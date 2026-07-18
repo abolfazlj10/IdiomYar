d7d6e13a8744904ff027efef7db91d0f5698589b
abolfazlj10 <abolfazljamshidi101010@gmail.com>
2026-07-18 22:21:46 +0330

    Add Vercel Analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
 import Providers from "./provider";
@@ -24,6 +25,7 @@ export default function RootLayout({
           </Providers>
           <Toaster />
         </div>
+        <Analytics />
       </body>
     </html>
   );
